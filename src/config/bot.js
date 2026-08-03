9e91b6ca3c9e625e812ddca82cdeb6da7caa501d
akhalaf22032010-cmd <akhalaf22032010@gmail.com>
2026-08-03 10:36:24 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Tuffer than ever -Miles",     // this is what people actually see
+        state: "Bot by Miles",     // this is what people actually see
         type: 4,               // Custom
       },
     ],

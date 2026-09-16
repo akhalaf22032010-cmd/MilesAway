e5cc471a24a93e758bd9a882be2c8c086b3db36f
akhalaf22032010-cmd <akhalaf22032010@gmail.com>
2026-09-16 10:36:34 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "JOIN LOOXMOG TODAY",     // this is what people actually see
+        state: "Free Guides in LOOXMOG.",     // this is what people actually see
         type: 4,               // Custom
       },
     ],

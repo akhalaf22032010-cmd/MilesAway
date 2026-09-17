e483c80574546761a911b01fd823e73fcb88c2f8
akhalaf22032010-cmd <akhalaf22032010@gmail.com>
2026-09-17 14:49:43 +0300

    Group shop-config under /shop as a subcommand

--- src/commands/Economy/shop.js
+++ src/commands/Economy/shop.js
@@ -1,13 +1,29 @@
 import { SlashCommandBuilder } from 'discord.js';
 import shopBrowse from './modules/shop_browse.js';
+import shopConfigSetrole from './modules/shop_config_setrole.js';
 
 export default {
     slashOnly: true,
     data: new SlashCommandBuilder()
         .setName('shop')
-        .setDescription('Browse the economy shop.'),
+        .setDescription('Browse the economy shop.')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('setrole')
+                .setDescription('Set the role granted for Premium Role purchases.')
+                .addRoleOption(option =>
+                    option
+                        .setName('role')
+                        .setDescription('The role to grant for Premium Role purchases.')
+                        .setRequired(true)
+                )
+        ),
 
     async execute(interaction, config, client) {
+        if (interaction.options.getSubcommand(false) === 'setrole') {
+            return shopConfigSetrole.execute(interaction, config, client);
+        }
+
         return shopBrowse.execute(interaction, config, client);
     },
 };

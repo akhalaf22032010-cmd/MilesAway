3876137a2ec172febed2dcb5eb15cb6812137a68
akhalaf22032010-cmd <akhalaf22032010@gmail.com>
2026-09-17 17:35:41 +0300

    Remove ticket command priority loader alias

--- src/commands/00_Ticket/ticket.js
+++
@@ -1,3 +0,0 @@
-// Load the existing ticket system early so it is retained when the bot has more than 100 commands.
-// The command loader de-duplicates commands by slash-command name.
-export { default } from '../Ticket/ticket.js';

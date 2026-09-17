1c958211b8892cd816476d4282fd47b2881bbd63
akhalaf22032010-cmd <akhalaf22032010@gmail.com>
2026-09-17 17:41:56 +0300

    Fix DNR event integration

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -309,7 +309,7 @@ async function handleLeveling(message, client) {
       finalXP = Math.floor(finalXP * levelingConfig.xpMultiplier);
     }
 
-    const result = await addXp(message.client, message.guild, message.member, finalXP);
+    const result = await addXp(client, message.guild, message.member, finalXP);
 
     if (result?.leveledUp) {
       logger.info(
